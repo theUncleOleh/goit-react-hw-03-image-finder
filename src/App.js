622ab2ed9e8dc4e5fc63d7c21b9bd79635327099
Oleh Semennykov <sumcue@gmail.com>
2022-02-16 22:29:51 +0200

    add loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Oval } from "react-loader-spinner";
 import "./App.css";
 import s from "./App.module.css";
 
@@ -12,19 +14,24 @@ class App extends PureComponent {
   state = {
     pictures: [],
     imageName: "",
+    loading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.imageName !== this.state.imageName) {
-      fetch(
-        `https://pixabay.com/api/?q=${this.state.imageName}&page=1&key=24437827-e20f686b1c65a4a2859f17630&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          this.setState((prevState) => ({
-            pictures: [...prevState.pictures, ...data.hits],
-          }));
-        });
+      this.setState({ loading: true });
+      setTimeout(() => {
+        fetch(
+          `https://pixabay.com/api/?q=${this.state.imageName}&page=1&key=24437827-e20f686b1c65a4a2859f17630&image_type=photo&orientation=horizontal&per_page=12`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            this.setState((prevState) => ({
+              pictures: [...prevState.pictures, ...data.hits],
+            }));
+          })
+          .finally(() => this.setState({ loading: false }));
+      }, 3000);
     }
   }
 
@@ -35,14 +42,19 @@ class App extends PureComponent {
   };
 
   render() {
-    const { pictures } = this.state;
+    const { pictures, loading } = this.state;
     return (
       <div>
         <SearchBar onSubmit={this.handleFormSubmit} />
 
         {/* галерея картинок */}
-
+        {loading && (
+          <div>
+            <Oval color="#00BFFF" height={80} width={80} />
+          </div>
+        )}
         <ImageGallery pictures={pictures} />
+
         {/* разметка модального окна */}
         <div class="overlay">
           <div class="modal">
